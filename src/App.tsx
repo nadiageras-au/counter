6c43d004a3438f10d84584fe13f2e9b591dd6e7f
nadia <nadiageras.au@gmail.com>
2024-01-02 15:21:14 +1000

    add local storage

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,36 @@ function App() {
 
     const minValue = 0;
     const maxValue = 5;
-    const [value, setValue  ] = useState(minValue)
+    const [value, setValue] = useState(minValue)
     const addCount = () => {
         if (value < maxValue)
-           setValue(prev => prev + 1)
+            setValue(prev => prev + 1)
     }
 
     const resetCount = () => {
         setValue(minValue)
+    }
+
+    const setToLocalStorage = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+    }
+
+    const getToLocalStorage = () => {
+       let valueAsString =  localStorage.getItem('counterValue');
+       if(valueAsString) {
+           let newValue = JSON.parse(valueAsString);
+           setValue(newValue)
+       }
+    }
+
+    const clearLocalStorage = () => {
+        localStorage.clear();
+    }
+
+
+    const removeItemFromStorage = () => {
+        localStorage.removeItem('counterValue + 1')
     }
 
     return (
@@ -26,8 +48,12 @@ function App() {
                 <Display countValue={value} maxValue={maxValue}/>
 
                 <div className='dflex block-btn'>
-                    <Button title="inc" isDisabled={value >= maxValue} onClick={()=>addCount()}/>
-                    <Button title="reset" isDisabled={value === minValue} onClick={()=>resetCount()}/>
+                    <Button title="inc" isDisabled={value >= maxValue} onClick={() => addCount()}/>
+                    <Button title="reset" isDisabled={value === minValue} onClick={() => resetCount()}/>
+                    <Button title="setToLocalStorage" isDisabled={false} onClick={() => setToLocalStorage()}/>
+                    <Button title="getToLocalStorage" isDisabled={false} onClick={() => getToLocalStorage()}/>
+                    <Button title="clearLocalStorage" isDisabled={false} onClick={() => clearLocalStorage()}/>
+                    <Button title="removeItemFromStorage" isDisabled={false} onClick={() => removeItemFromStorage()}/>
                 </div>
             </div>
 
